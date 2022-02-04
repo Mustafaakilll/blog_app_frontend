63f882bef4ce275c46c02279dac2665ecad1269e
apooness <mustafaakil49@gmail.com>
2022-02-04 03:20:48 +0300

    Sample Views added for pages

---
+++ lib/session/add_article/add_article_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddArticleView extends StatelessWidget {
+  const AddArticleView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('ADD ARTICLE'),
+      ),
+    );
+  }
+}
