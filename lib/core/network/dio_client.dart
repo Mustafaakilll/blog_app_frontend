5114384914337655f0bbc6b40b20d10b86c8a663
Mustafaakilll <mustafaakil49@gmail.com>
2022-05-15 22:18:18 +0300

    Dio client seperated to named constructors

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -2,19 +2,27 @@ import 'package:dio/dio.dart' hide LogInterceptor;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'interceptors/auth_interceptor.dart';
-import 'interceptors/cache_interceptor.dart';
-import 'interceptors/log_interceptor.dart';
 
-class DioClient {
-  DioClient() {
-    dio.options = BaseOptions(baseUrl: dotenv.get('BASE_URL'));
-    _dio.interceptors
-      ..add(LogInterceptor())
-      ..add(AuthInterceptor())
-      ..add(CacheInterceptor());
+class BaseDioClient {
+  BaseDioClient.auth() {
+    _dio.options.baseUrl = dotenv.get('AUTH_ENDPOINT');
+  }
+
+  BaseDioClient.user() {
+    _dio.options.baseUrl = dotenv.get('USER_ENDPOINT');
+  }
+
+  BaseDioClient.article() {
+    _dio.options.baseUrl = dotenv.get('ARTICLE_ENDPOINT');
+  }
+
+  BaseDioClient.cloudinary() {
+    final cloudinaryUrl = dotenv.get('CLOUDINARY_URL');
+    final cloudName = dotenv.get('CLOUD_NAME');
+    _dio.options.baseUrl = '$cloudinaryUrl$cloudName';
   }
 
   final Dio _dio = Dio();
 
-  Dio get dio => _dio;
+  Dio get dio => _dio..interceptors.add(AuthInterceptor());
 }
